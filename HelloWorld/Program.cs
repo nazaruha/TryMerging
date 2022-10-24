5b7bf68e4f01dde2ab8e55adf5e7f5aa255b5ced
Nazar <fedunnazar20122001@gmail.com>
2022-10-24 19:16:51 +0300

    nazar_master commit.1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("nazar_master commit");
+            Console.WriteLine("nazar_master commit2");
         }
     }
 }
